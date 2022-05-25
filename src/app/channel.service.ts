199a44445b95fe8fffb7232212fef761ca32d027
Abhijit Patil <abhijit@caizin.com>
2022-05-25 12:21:44 +0530

    second commit

--- src/app/channel.service.ts
+++ src/app/channel.service.ts
@@ -21,19 +21,16 @@ export class channelService{
         this.arrChannel.push(ch);
     }
     addUser(name:string){
-        console.log("current:"+this.currentChannel);
-        console.log(this.arrChannel.find((elem)=>elem.name==this.currentChannel));
         this.arrChannel.find((elem)=>elem.name==this.currentChannel)?.arrSubUsers.push(name);
-        console.log(this.arrChannel.find((elem)=>elem.name==this.currentChannel));
+       
     }
     addMessage(msg:string,uname:string){
         let m=new message();
         m.messageBody=msg;
         m.messageDate=new Date();
         m.userName=uname;
-        console.log(m);
         this.arrChannel.find((elem)=>elem.name==this.currentChannel)?.arrMessage.push(m);
-        console.log(this.arrChannel.find((elem)=>elem.name==this.currentChannel))
+        
     }
 
     getChannels():Channel[]{
